f8b9fb9906f24eeb3410322a61dafc5d80eba111
arindammangal19 <50660615+arindammangal19@users.noreply.github.com>
2021-04-30 10:43:22 +0530

    Create info1.php

---
+++ info1.php
@@ -0,0 +1,20 @@
+
+
+	<?php
+	
+	   class demo
+	   {
+	       private $a = "hello javatpoint";
+	       
+	       public function display()
+	       {
+	           echo "<h2 style='color:#5f1251'>".$this->a;
+	       }
+	       
+	   }
+	
+	      $obj = new demo();
+	      
+	        $obj->display();
+	        
+	?>
